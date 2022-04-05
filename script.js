f88bedbac86bac65cbb4192ff9efa41a09bb361a
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-04-05 18:07:09 +0200

    style adjustments

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ h3s = document.querySelectorAll("h3")
 hideContent()
 
 h2s.forEach((h2) => {
-    h2.style.color = randDarkColor()
     h2.addEventListener("click", () => {  
         if (h2.nextElementSibling.classList.contains("do-not-display")) {
             hideContent()
@@ -41,18 +40,3 @@ function hideDetails() {
     })
 }
 
-function randDarkColor() {
-    var lum = -0.25;
-    var hex = String('#' + Math.random().toString(16).slice(2, 8).toUpperCase()).replace(/[^0-9a-f]/gi, '');
-    if (hex.length < 6) {
-        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-    }
-    var rgb = "#",
-        c, i;
-    for (i = 0; i < 3; i++) {
-        c = parseInt(hex.substr(i * 2, 2), 16);
-        c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
-        rgb += ("00" + c).substr(c.length);
-    }
-    return rgb;
-}
