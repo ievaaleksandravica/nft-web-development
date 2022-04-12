39d18f89e2090c86be3ad436243d9ffa6b479138
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-04-12 14:53:15 +0200

    pip3 install notebook

--- crypto-token/Crypto-Token-Contract.sol
+++ crypto-token/Crypto-Token-Contract.sol
@@ -1,4 +1,8 @@
 // pragma solidity defines our compiler version for Solidity
+// the uptick arrow defines a range compile version after the non zero digit upwards to the next increment of the non zero number
 pragma solidity ^0.8.4;
 
-// the uptick arrow defines a range compile version after the non zero digit upwards to the next increment of the non zero number
+// in Solidity everyhing is explicitly declared
+contract Token {
+    // all the code to write smart contracts go in here
+}
