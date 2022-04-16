b0c13f70893d275b3ecad9d58541cb41f1cc3e36
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-04-16 10:20:28 +0200

    Three main types of variables

---
+++ solidity-practice/types.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Types {
+    // to check if it is true or false
+    bool public lieDetector;
+
+    // integer to hold the amount
+    uint256 public walletAmount;
+
+    // store text information about the error
+    string public errorMessage = "there has been a mistake!";
+}
