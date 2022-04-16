4ef3c39518b4aa4af95444b48ac5363955e9343a
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-04-16 10:14:03 +0200

    Variables

---
+++ solidity-practice/variables.sol
@@ -0,0 +1,7 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract chocolate {
+    uint256 public chocolateBar = 7;
+    uint256 public storeOwner = 300;
+    uint256 public totalValue = storeOwner + chocolateBar;
+}
