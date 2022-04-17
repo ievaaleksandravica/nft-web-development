bfd63e63a1874917810da3c1618ad34d07682088
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-04-17 18:50:46 +0200

    storeOwner

--- solidity-practice/learnVariables.sol
+++ solidity-practice/learnVariables.sol
@@ -3,5 +3,6 @@ pragma solidity >=0.7.0 <0.9.0;
 contract learnVariables {
     // all our solidity code
 
-    uint256 chocolateBar;
+    uint256 chocolateBar = 10;
+    uint256 storeOwner = 300;
 }
