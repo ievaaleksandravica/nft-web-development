3532b93b54702be9bb7042522f911de1045ebc17
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-04-20 12:59:06 +0200

    solution

--- solidity-practice/decisionMaking.sol
+++ solidity-practice/decisionMaking.sol
@@ -1,13 +1,24 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract decisionMaking {
-    uint256 oranges = 5;
+    uint256 public oranges = 5;
+    uint256 public stakingWallet = 10;
 
     function validateOranges() public view returns (bool) {
         if (oranges == 5) {
             return true;
         } else {
             return false;
+        }
+    }
+
+    function airDrop() public view nonpayable returns (uint256) {
+        if (stakingWallet == 10) {
+            stakingWallet = stakingWallet + 10;
+            return stakingWallet;
+        } else {
+            stakingWallet = stakingWallet + 1;
+            return stakingWallet;
         }
     }
 }
