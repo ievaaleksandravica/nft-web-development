63ebca69b0f305e564bd39eeb04077da70af7d3e
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-04-20 18:29:42 +0200

    challenge

--- solidity-practice/arrays.sol
+++ solidity-practice/arrays.sol
@@ -4,6 +4,7 @@ contract learnArrays {
     uint256[] public myArray;
     uint256[] public myArray2;
     uint256[200] public myFixedSizedArray;
+    uint256[] public changeArray;
 
     // push()
     function push(uint256 number) public {
@@ -20,5 +21,19 @@ contract learnArrays {
 
     function remove(uint256 i) public {
         delete myArray[i];
+    }
+
+    function removeElement(uint256 i) public {
+        uint256 lastIndex = myArray.length - 1;
+        myArray[i] = myArray[lastIndex];
+        myArray[lastIndex] = myArray[i];
+        myArray.pop();
+    }
+
+    function test() public {
+        changeArray.push(1);
+        changeArray.push(2);
+        changeArray.push(3);
+        changeArray.push(4);
     }
 }
