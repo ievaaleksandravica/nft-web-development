a19e34dc248f20c8b04ca9968a8208a2ecb00630
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-04-20 17:54:52 +0200

    Create arrays

--- solidity-practice/arrays.sol
+++ solidity-practice/arrays.sol
@@ -1,3 +1,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
-contract learnArrays {}
+contract learnArrays {
+    uint256[] public myArray;
+    uint256[] public myArray2;
+    uint256[200] public myFixedSizedArray;
+}
